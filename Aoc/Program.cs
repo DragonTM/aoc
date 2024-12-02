ed3b041e28e4f3e6fc29c8403804a332406ed80d
ivan.kushnir <ivan_kushnir@icloud.com>
2024-12-03 00:36:44 +0200

    aoc 2024 task 1.2

--- Aoc/Program.cs
+++ Aoc/Program.cs
@@ -4,38 +4,21 @@ using var inputFile = new StreamReader("input.txt");
 
 var line = inputFile.ReadLine();
 var left = new List<int>();
-var right = new List<int>();
+var right = new Dictionary<int, int>();
 
 while (!string.IsNullOrEmpty(line))
 {
     var numbers = line.Split(" ").Where(v => !string.IsNullOrEmpty(v)).Select(v => int.Parse(v)).ToArray();
 
-    if (left.Count < 1)
+    left.Add(numbers[0]);
+    
+    if (right.ContainsKey(numbers[1]))
     {
-        left.Add(numbers[0]);
-        right.Add(numbers[1]);
+        right[numbers[1]]++;
     }
     else
     {
-        var addLeftAdd = FindPlace(left, numbers[0]);
-        if (addLeftAdd < left.Count)
-        {
-            left.Insert(addLeftAdd, numbers[0]);
-        }
-        else
-        {
-            left.Add(numbers[0]);
-        }
-
-        var addRightAdd = FindPlace(right, numbers[1]);
-        if (addRightAdd < right.Count)
-        {
-            right.Insert(addRightAdd, numbers[1]);
-        }
-        else
-        {
-            right.Add(numbers[1]);
-        }
+        right.Add(numbers[1], 1);
     }
 
     line = inputFile.ReadLine();
@@ -45,20 +28,11 @@ var result = 0;
 
 for (var i = 0; i < left.Count; i++)
 {
-    result += Math.Abs(left[i] - right[i]);
+    if (right.TryGetValue(left[i], out var count)){
+        result += count * left[i];
+    }
 }
 
 using var outputFile = new StreamWriter("output.txt", false);
 
 outputFile.Write(result);
-
-int FindPlace(List<int> list, int value)
-{
-    var i = 0;
-    while (i < list.Count && list[i] < value )
-    {
-        i++;
-    }
-
-    return i;
-}
